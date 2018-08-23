6b673eb01270e02ff3d5b3be0c4fe25a402cccb3
afaraone <andresfaraone@outlook.com>
2018-08-23 15:39:43 +0100

    Make spec test multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,27 @@
 require  'fizzbuzz'
 
+
 describe 'fizzbuzz_says' do
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz_says(15)).to eq 'fizzbuzz'
-  end
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz_says(3)).to eq 'fizz'
-  end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz_says(5)).to eq 'buzz'
+  context 'knows when a number is' do
+    it 'is divisible by 3' do
+      expect(is_divisible_by?(3,3)).to eq true
+    end
   end
 end
 
-describe 'fizzbuzz' do
-  it 'is divisible by 3' do
-    expect(is_divisible_by(3)).to eq 0
-  end
-end
+
+#it 'returns "fizzbuzz" when passed multiple of 15' do
+  #expect(fizzbuzz_says(15)).to eq 'fizzbuzz'
+#end
+#it 'returns "fizz" when passed 3' do
+  #expect(fizzbuzz_says(3)).to eq 'fizz'
+#end
+#it 'returns "buzz" when passed 5' do
+  #expect(fizzbuzz_says(5)).to eq 'buzz'
+#end
+#
+#describe 'fizzbuzz' do
+  #it 'is divisible by 3' do
+    #expect(is_divisible_by(3)).to eq 0
+  #end
+#end
